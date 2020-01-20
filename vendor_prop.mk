Keerten/device_xiaomi_whyred

whyred: Set property procedure_bytes to SKIP
Set "persist.vendor.radio.procedure_bytes" property value
to SKIP for testIccTransmitApduLogicalChannel to pass

Change-Id: I117dd9ec6976d35e23ce10158e1ef4706e17104f
CRs-Fixed: 2491738

vendor_prop.mk
@@ -165,5 +165,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.data_ltd_sys_ind=1 \
+    persist.vendor.radio.procedure_bytes=SKIP \
     persist.radio.calls.on.ims=1 \
     persist.dbg.wfc_avail_ovr=1

BlissRoms-Devices/android_device_xiaomi_whyred

vendor_prop.mk
@@ -154,4 +154,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.rat_on=combine
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.procedure_bytes=SKIP